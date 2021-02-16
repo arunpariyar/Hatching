2ccbf22263ba6bd922d2ac72b01939d2030c7934
Arun Pariyar <arun.pariyar@gmail.com>
2021-02-16 13:21:58 +0100

    fix add optimised images and added sticky navigation

--- Portfolio Projects/Destination-Annapurna/scripts/script.js
+++ Portfolio Projects/Destination-Annapurna/scripts/script.js
@@ -1,15 +1,33 @@
-//SMOOTH SCROLLING
-const btntotrips = document.querySelector(".btntotrips");
-const sectionTrips = document.querySelector(".section--1");
+const btnToTrips = document.querySelector(".btnToTrips");
+const sectionTrips = document.getElementById("section--1");
 
-btntotrips.addEventListener("click", function (e) {
-  console.log("btntotrips clicked");
+btnToTrips.addEventListener("click", () => {
+  console.log("button clicked");
   sectionTrips.scrollIntoView({ behavior: "smooth" });
 });
 
-const btnToAdventures = document.querySelector(".btnToAdventures");
+// Sticky Navigation using intersection observer -----------------------------
+//selecting the navi element
+const navi = document.querySelector(".nav");
 
-btnToAdventures.addEventLister('click', function() {
-  sectionTrips.scrollIntoView({ behavior: "smooth" });
+//creating the function that makes the Navi Sticky
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    navi.classList.add("sticky");
+  } else {
+    navi.classList.remove("sticky");
+  }
 };
 
+//Header Element
+const header = document.querySelector(".header");
+
+//creating a header observer using IntersectionObserver API
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+});
+
+//initiating the header Observer on the header
+headerObserver.observe(header);
