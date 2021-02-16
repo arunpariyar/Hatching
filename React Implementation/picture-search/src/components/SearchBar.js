6595a413f3cd0eb5c931dfb3fd75547b92fd9534
Arun Pariyar <arun.pariyar@gmail.com>
2021-02-16 18:19:02 +0100

    feat add a search box on the app

--- React Implementation/picture-search/src/components/SearchBar.js
+++ React Implementation/picture-search/src/components/SearchBar.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: " " };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
   render() {
     return (
       <div class="ui segment">
-        <form className="ui form">
-          <label> Image Search</label>
-          <input type="text" />
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <label>Image Search</label>
+          <input
+            type="text"
+            value={this.state.term}
+            onChange={(e) => this.setState({ term: e.target.value })}
+          />
         </form>
       </div>
     );
