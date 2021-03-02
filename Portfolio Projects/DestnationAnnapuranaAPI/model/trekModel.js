437ff1d03e2209c5b540e6fe6ebc65ef723a568c
Arun Pariyar <arun.pariyar@gmail.com>
2021-03-02 11:17:37 +0100

    feat correct data model and script to seed the database

--- Portfolio Projects/DestnationAnnapuranaAPI/model/trekModel.js
+++ Portfolio Projects/DestnationAnnapuranaAPI/model/trekModel.js
@@ -5,11 +5,46 @@ const trekSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A trek must have a name'],
     unique: true,
+    trim: true,
   },
-  summary: String,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  elevation: {
+    type: Number,
+    required: [true, 'A trek must have an elevation'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A trek must have duration'],
+  },
+  difficulty: {
+    type: String,
+    require: [true, 'A trek must have difficulty level'],
+    trim: true,
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'a trek must have a group size'],
+  },
   price: {
     type: Number,
     required: [true, 'A trek must have a price'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A trek must have cover image'],
+  },
+  images: [String],
+  startDates: [Date],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 //creating a Model using the above mongoose Sehema
