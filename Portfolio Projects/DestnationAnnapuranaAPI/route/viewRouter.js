e3cb1ac9e63d51c9b037ec938079bcf49ef2612b
Arun Pariyar <arun.pariyar@gmail.com>
2021-05-03 09:35:35 +0200

    fix removed Nextjs folder

--- Portfolio Projects/DestnationAnnapuranaAPI/route/viewRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const viewController = require('../controllers/viewController');
-
-router.get('/', viewController.getHomepage);
-router.get('/trek', viewController.getTrek);
-
-module.exports = router;
